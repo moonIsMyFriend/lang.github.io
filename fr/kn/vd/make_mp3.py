772789018a1c75659b18c48bab6a3fd3add5b363
inht <timpark001@gmail.com>
2026-04-12 17:32:17 +0900

    영상프랑스어

--- fr/kn/vd/make_mp3.py
+++ fr/kn/vd/make_mp3.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from shutil import make_archive
 
 titles = ['00.num.0_20', '01.num.21_40', '02.num.41_60', '03.num.61_80', '04.num.81_100', '15']
-titles = ['05_1', '05_2']
+titles = ['08_2']
 print('making', titles)
 
 
