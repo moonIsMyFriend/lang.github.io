04c1abaf447a9bf8ebbce502ea6eb143a9ff31a8
inht <timpark001@gmail.com>
2026-05-05 17:00:00 +0900

    12강

--- fr/kn/pron/make_mp3.py
+++ fr/kn/pron/make_mp3.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from shutil import make_archive
 
 titles = ['00.num.0_20', '01.num.21_40', '02.num.41_60', '03.num.61_80', '04.num.81_100', '15']
-titles = ['11. 자음 t,d,k,g']
+titles = ['12. 자음 s,z,n,ɲ']
 print('making', titles)
 
 
