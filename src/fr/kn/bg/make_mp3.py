88dae91f60f630b2c3a7657a03f461109446f317
inht <timpark001@gmail.com>
2026-06-13 09:08:24 +0900

    15

--- src/fr/kn/bg/make_mp3.py
+++ src/fr/kn/bg/make_mp3.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from shutil import make_archive
 
 titles = ['00.num.0_20', '01.num.21_40', '02.num.41_60', '03.num.61_80', '04.num.81_100', '15']
-titles = ['00-6']
+titles = ['00-12', '00-15']
 
 BASE_DIR = "./"          # 현재 폴더만 스캔
 EXT      = ".csv"
